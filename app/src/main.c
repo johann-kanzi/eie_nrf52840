b2eb224f5a5a3cc5379eedcccec569af189e6313
johann-kanzi <johannyap90@gmail.com>
2026-01-18 18:41:59 -0700

    created simple button stuff

--- app/src/main.c
+++ app/src/main.c
@@ -2,27 +2,35 @@
  * main.c
  */
 
+#include <zephyr/kernel.h>
+#include <zephyr/device.h>
+#include <zephyr/drivers/gpio.h>
+#include <zephyr/sys/printk.h>
 #include <inttypes.h>
 
-#include <zephyr/kernel.h>
-#include <zephyr/sys/printk.h>
+#define SLEEP_TIME_MS 1000
 
-#include "BTN.h"
-#include "LED.h"
-
-#define SLEEP_MS 1
+#define SW0_NODE  DT_ALIAS(sw0)
+static const struct gpio_dt_spec button = GPIO_DT_SPEC_GET(SW0_NODE, gpios);
 
 int main(void) {
+    int ret;
 
-  if (0 > BTN_init()) {
-    return 0;
-  }
-  if (0 > LED_init()) {
-    return 0;
-  }
+    if (!gpio_is_ready_dt(&button)) {
+        return 0;
+    }
 
-  while(1) {
-    k_msleep(SLEEP_MS);
-  }
-	return 0;
+    ret = gpio_pin_configure_dt(&button, GPIO_INPUT);
+    if (0 > ret) {
+        return 0;
+    }
+
+    while(1) {
+        ret = gpio_pin_get_dt(&button);
+        if (0 < ret) {
+            printk("Pressed!\n");
+        }
+        k_msleep(SLEEP_TIME_MS);
+    }
+    return 0;
 }
