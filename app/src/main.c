23be21cda13dd674bd9cbd436f3435c50a4e3719
johann-kanzi <johannyap90@gmail.com>
2026-01-18 19:55:48 -0700

    yada yada api used for led and button

--- app/src/main.c
+++ app/src/main.c
@@ -3,34 +3,29 @@
  */
 
 #include <zephyr/kernel.h>
-#include <zephyr/device.h>
-#include <zephyr/drivers/gpio.h>
-#include <zephyr/sys/printk.h>
-#include <inttypes.h>
 
-#define SLEEP_TIME_MS 1000
+#include "BTN.h"
+#include "LED.h"
 
-#define SW0_NODE  DT_ALIAS(sw0)
-static const struct gpio_dt_spec button = GPIO_DT_SPEC_GET(SW0_NODE, gpios);
+#define SLEEP_MS 1
 
 int main(void) {
-    int ret;
 
-    if (!gpio_is_ready_dt(&button)) {
+    if (0 > BTN_init()) {
         return 0;
     }
 
-    ret = gpio_pin_configure_dt(&button, GPIO_INPUT);
-    if (0 > ret) {
+    if (0 > LED_init()) {
         return 0;
     }
 
     while(1) {
-        ret = gpio_pin_get_dt(&button);
-        if (0 < ret) {
-            printk("Pressed!\n");
+        /* Checks if the button was pressed and clears the flag if it was */
+        if (BTN_check_clear_pressed(BTN0)) {
+            LED_toggle(LED0);
+            printk("Button 0 pressed!\n");
         }
-        k_msleep(SLEEP_TIME_MS);
+        k_msleep(SLEEP_MS);
     }
     return 0;
 }
